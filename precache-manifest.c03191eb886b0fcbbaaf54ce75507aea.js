5fa173a2303d540f3f88beadd5d3d234c92696b4
WIN-53DO7REO2CI\user <.>
2025-06-04 22:32:27 +0530

    Updates

---
+++ precache-manifest.c03191eb886b0fcbbaaf54ce75507aea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ac1ec8fbfff178c3fb9ea16d1a31ba5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8903ce9494e843ad0a05",
+    "url": "/static/css/main.427275ce.chunk.css"
+  },
+  {
+    "revision": "4a4485690fe0b2554e2b",
+    "url": "/static/js/2.59d474c0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.59d474c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8903ce9494e843ad0a05",
+    "url": "/static/js/main.38d8a8f0.chunk.js"
+  },
+  {
+    "revision": "35f67668c217d61057f5",
+    "url": "/static/js/runtime-main.ed9d8f35.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
